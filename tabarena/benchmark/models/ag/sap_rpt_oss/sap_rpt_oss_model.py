00a85bd2e26999fa33ef10cbe8eb2b60a0d81411
LennartPurucker <contact@lennart-purucker.com>
2025-11-17 20:25:54 +0100

    fix: make sap rpt oss run

--- tabarena/benchmark/models/ag/sap_rpt_oss/sap_rpt_oss_model.py
+++ tabarena/benchmark/models/ag/sap_rpt_oss/sap_rpt_oss_model.py
@@ -3,17 +3,22 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
+import torch
 from autogluon.common.utils.resource_utils import ResourceManager
+from autogluon.core.constants import REGRESSION
 from autogluon.core.models import AbstractModel
+from torch import nn
+from torch.nn.attention import SDPBackend, sdpa_kernel
 
 if TYPE_CHECKING:
+    import numpy as np
     import pandas as pd
+
 
 logger = logging.getLogger(__name__)
 
-# FIXME: the model uses some kind of parallel backend that does not work on shared hardware
-# FIXME: model is not pickable
-# FIXME: predict proba output is weird
+
+
 class SAPRPTOSSModel(AbstractModel):
     """ConTextTab Model: https://github.com/SAP-samples/sap-rpt-1-oss."""
 
@@ -38,6 +43,10 @@ class SAPRPTOSSModel(AbstractModel):
                 f"Fit specified to use {num_gpus} GPU, but only {available_num_gpus} "
                 "CUDA GPUs are available. Please activate CUDA or switch to CPU usage.",
             )
+
+        # FIXME: remove workaround and integrate into codebase 
+        from sap_rpt_oss.model import attention
+        attention.TorchSelfAttention = TorchSelfAttention
 
         from sap_rpt_oss import SAP_RPT_OSS_Classifier, SAP_RPT_OSS_Regressor
 
@@ -73,6 +82,16 @@ class SAPRPTOSSModel(AbstractModel):
         }
         for param, val in default_params.items():
             self._set_default_param_value(param, val)
+
+    def _predict_proba(self, X, **kwargs) -> np.ndarray:
+        X = self.preprocess(X, **kwargs)
+
+        if self.problem_type == REGRESSION:
+            # FIXME: make model code flatten by default as normal for sklearn models
+            return self.model.predict(X).flatten()
+
+        y_pred_proba = self.model.predict_proba(X)
+        return self._convert_proba_to_unified_form(y_pred_proba)
 
     @classmethod
     def supported_problem_types(cls) -> list[str] | None:
@@ -134,3 +153,78 @@ def pre_download_model():
     hf_hub_download(
         repo_id="SAP/sap-rpt-1-oss", filename="2025-11-04_sap-rpt-one-oss.pt"
     )
+
+
+# FIXME: model is not pickable -> _SDPBackend -> make pickable with code below
+#   Comes from sap_rpt_oss.model.attention.TorchSelfAttention
+#         if config.attention_implementation == 'efficient' and torch.cuda.is_available():
+#             self.backend = SDPBackend.EFFICIENT_ATTENTION
+#         elif config.attention_implementation == 'math' or not torch.cuda.is_available():
+#             self.backend = SDPBackend.MATH
+class TorchSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(
+            config, "embedding_size"
+        ):
+            raise ValueError(
+                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
+                f"heads ({config.num_attention_heads})"
+            )
+
+        self.num_attention_heads = config.num_attention_heads
+        assert config.hidden_size % config.num_attention_heads == 0, (
+            f"{config.hidden_size=} must be divisible by {config.num_attention_heads=}"
+        )
+        self.attention_head_size = config.hidden_size // config.num_attention_heads
+
+        self.query = nn.Linear(config.hidden_size, config.hidden_size)
+        self.key = nn.Linear(config.hidden_size, config.hidden_size)
+        self.value = nn.Linear(config.hidden_size, config.hidden_size)
+
+        self.dropout = config.attention_probs_dropout_prob
+
+        if config.attention_implementation == "efficient" and torch.cuda.is_available():
+            self.backend = "EF"
+        elif config.attention_implementation == "math" or not torch.cuda.is_available():
+            self.backend = "SDP"
+        else:
+            raise ValueError(
+                f"Unknown attention implementation {config.attention_implementation}"
+            )
+
+    def transpose_for_scores(self, x: torch.Tensor) -> torch.Tensor:
+        new_x_shape = (*x.size()[:-1], self.num_attention_heads, self.attention_head_size)
+        x = x.view(new_x_shape)
+        return x.permute(0, 2, 1, 3)  # (batch_size, num_heads, seq_len, head_dim)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: torch.FloatTensor | None = None,
+    ) -> torch.Tensor:
+        """hidden_states: shape (batch_size, seq_len, hidden_size)
+        attention_mask: shape (batch_size, seq_len, seq_len) or (batch_size, 1, seq_len, seq_len).
+        """
+        query_layer = self.transpose_for_scores(self.query(hidden_states))
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+
+        backend = (
+            SDPBackend.EFFICIENT_ATTENTION if self.backend == "EF" else SDPBackend.MATH
+        )
+
+        with sdpa_kernel(backend):
+            attn_output = torch.nn.functional.scaled_dot_product_attention(
+                query_layer,
+                key_layer,
+                value_layer,
+                attn_mask=attention_mask,
+                dropout_p=self.dropout if self.training else 0.0,
+                is_causal=False,
+            )
+
+        context_layer = attn_output.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = (*context_layer.size()[:-2], self.config.hidden_size)
+        return context_layer.view(new_context_layer_shape)
