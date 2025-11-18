905709744a9a0d3e5ef2ea40cbd9b12f7235902f
LennartPurucker <contact@lennart-purucker.com>
2025-11-19 00:01:28 +0100

    fix import error

--- tst/benchmark/models/test_tabpfnv25.py
+++ tst/benchmark/models/test_tabpfnv25.py
@@ -13,14 +13,14 @@ def test_tabpfnv25():
         from tabarena.benchmark.models.ag.tabpfnv2_5.tabpfnv2_5_model import (
             RealTabPFNv25Model,
         )
+
+        model_cls = RealTabPFNv25Model
+        FitHelper.verify_model(
+            model_cls=model_cls, model_hyperparameters=model_hyperparameters
+        )
     except ImportError as err:
         pytest.skip(
             f"Import Error, skipping test... "
             f"Ensure you have the proper dependencies installed to run this test:\n"
             f"{err}"
-        )
-    else:
-        model_cls = RealTabPFNv25Model
-        FitHelper.verify_model(
-            model_cls=model_cls, model_hyperparameters=model_hyperparameters
         )
