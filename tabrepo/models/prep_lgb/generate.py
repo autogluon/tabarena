6a5e101861746d16ac12d709ad5cad362c4d8da5
atschalz <andrej.tschalzev@uni-mannheim.de>
2025-11-18 15:13:57 +0100

    Update PrepGBM: Add residual logic and preprocessors, switch to use LGBModel as superclass; Adapt search space

--- tabrepo/models/prep_lgb/generate.py
+++ tabrepo/models/prep_lgb/generate.py
@@ -7,7 +7,28 @@ from tabrepo.benchmark.experiment import YamlExperimentSerializer
 from tabrepo.models.utils import convert_numpy_dtypes
 from tabrepo.utils.config_utils import CustomAGConfigGenerator
 
-from tabprep.presets.lgb_presets import get_lgb_presets
+import numpy as np
+
+'''
+Todos:
+- Adjust num_leaves
+- Check whether we still need extra_trees if we have linear init
+- Add arithmetic
+- Add linear init
+- Add cat FE
+- Add OOF TE
+'''
+
+cat_int_kwargs = {'max_order': 3, 'min_cardinality': 2, 'add_freq': False, 'use_filters': False, 'max_base_interactions': 100}
+residual_init_kwargs = {'scaler': 'squashing', 'linear_model_type': 'lasso', 'lambda_': 'medium', 'cat_method': 'ohe'}
+
+def normal_pdf_points(k, border_density=0.05):
+    a = np.sqrt(-2*np.log(border_density*np.sqrt(2*np.pi)))
+    x = np.linspace(-a, a, k)
+    y = (1/np.sqrt(2*np.pi)) * np.exp(-0.5 * x**2)
+    return list(y)
+
+num_leaves = [8, 16, 32, 64, 128, 256, 512]
 
 def generate_configs_lightgbm(num_random_configs=200) -> list:
     search_space = ConfigurationSpace(
@@ -16,7 +37,8 @@ def generate_configs_lightgbm(num_random_configs=200) -> list:
             Float("feature_fraction", (0.4, 1.0)),
             Float("bagging_fraction", (0.7, 1.0)),
             Categorical("bagging_freq", [1]),
-            Integer("num_leaves", (2, 200), log=True),
+            # Integer("num_leaves", (2, 200), log=True),
+            Categorical("num_leaves", num_leaves, weights=normal_pdf_points(len(num_leaves), border_density=0.10)),
             Integer("min_data_in_leaf", (1, 64), log=True),
             Categorical("extra_trees", [False, True]),
             # categorical hyperparameters
@@ -28,15 +50,20 @@ def generate_configs_lightgbm(num_random_configs=200) -> list:
             Float("lambda_l1", (1e-4, 1.0)),
             Float("lambda_l2", (1e-4, 2.0)),
             # could search max_bin but this is expensive
+
+            # Preprocessing hyperparameters
+            Categorical("use_arithmetic_preprocessor", [True, False]),
+            Categorical("use_linear_residuals", [True, False]),
+            Categorical("use_cat_fe", [True, False]),
         ],
         seed=1234,
     )
-    
+
     configs = search_space.sample_configuration(num_random_configs)
     if num_random_configs == 1:
         configs = [configs]
     configs = [dict(config) for config in configs]
-    
+
     r_num = 1
     for i in range(len(configs)):
         if 'prep_params' not in configs[i]:
@@ -44,25 +71,47 @@ def generate_configs_lightgbm(num_random_configs=200) -> list:
         if 'preset_name' not in configs[i]:
             configs[i]['preset_name'] = f'r{r_num}'
             r_num += 1
+        if configs[i].pop('use_arithmetic_preprocessor') == True:
+            configs[i]['prep_params'].update({
+                'ArithmeticPreprocessor': {'cat_as_num': False}
+            })
+        if configs[i].pop('use_linear_residuals') == True:
+            configs[i]['use_residuals'] = True
+            configs[i]['residual_type'] = 'oof'
+            configs[i]['max_dataset_size_for_residuals'] = 1000
+            configs[i]['residual_init_kwargs'] = residual_init_kwargs
+
+        if configs[i].pop('use_cat_fe') == True:
+            configs[i]['prep_params'].update({
+                'CatIntAdder': cat_int_kwargs, 
+                'OOFTargetEncoder': {'alpha': 10}
+                        })
+
     return [convert_numpy_dtypes(config) for config in configs]
 
-presets = get_lgb_presets()
-manual_configs = []
 
-for k, v in presets.items():
-    v.update({'preset_name': k})
-    manual_configs.append(v)
-
-gen_prep_lightgbm = CustomAGConfigGenerator(
+gen_lightgbm = CustomAGConfigGenerator(
     model_cls=PrepLGBModel,
     search_space_func=generate_configs_lightgbm,
-    manual_configs=manual_configs,
+    manual_configs=[
+        {
+        'preset_name': 'arithmetic_catfe_oofte_linresoof',
+        'prep_params': {
+            'ArithmeticPreprocessor': {'cat_as_num': False},
+            'CatIntAdder': cat_int_kwargs, 
+            'OOFTargetEncoder': {'alpha': 10}
+                    },
+        'use_residuals': True,
+        'residual_type': 'oof',
+        'max_dataset_size_for_residuals': 1000,
+        'residual_init_kwargs': residual_init_kwargs,
+        },
+    ],
 )
 
-
 if __name__ == "__main__":
-    experiments = gen_prep_lightgbm.generate_all_bag_experiments(num_random_configs=25)
+    experiments = gen_lightgbm.generate_all_bag_experiments(num_random_configs=200)
     YamlExperimentSerializer.to_yaml(
-        experiments=experiments, path="configs_prep_lightgbm_alt.yaml"
+        experiments=experiments, path="configs_lightgbm_alt.yaml"
     )
-    print(f"Generated {len(experiments)} experiments.")
+
