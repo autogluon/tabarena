95cfb4a1706e67ba2977e3b0e16daf31b057fb18
Lennart Purucker <contact@lennart-purucker.com>
2025-12-10 21:40:45 +0100

    [WIP] [New Model] SAP-RPT-OSS / ConTexTab (#244)
    * add: tabpfn25 as update to tabpfn2
    
    * maint: rename files
    
    * fix/add: benchmark with slurm for TabPFNv25
    
    * minor fix
    
    * minor fixes
    
    * fix: new random_seed_logic and some improvements to memory estimates
    
    * add: support for better plotting of many models
    
    * maint: make RealTabPFN a subclass
    
    * fix: slurm memory
    
    * add: output csv with scores per model
    
    * fix: add time limit to TabPFN code
    
    * fix: add fit time limit
    
    * add: disable adaptive tree for many features
    
    * maint/refactor: remove RF PFN and simplify search space
    
    * maint: revert to old fetch pickles logics
    
    * maint: minor fixes to SLURM benchmarking
    
    * add: final slurm settings for TabPFN v2.5 eval
    
    * add: init of sap rpt oss
    
    * fix: make output flexible
    
    * add: todos for devs
    
    * fix: make sap rpt oss run
    
    * fix: make sap rpt oss run
    
    * fix: finalize test
    
    * adjust pr changes to new repo structure
    
    * fix import error
    
    * refactor to new repo state and add test
    
    * add latest state, add logic to disable preprocessing and give model raw data
    
    * add final slurm and eval state

--- tabarena/tabarena/benchmark/models/ag/__init__.py
+++ tabarena/tabarena/benchmark/models/ag/__init__.py
@@ -4,6 +4,7 @@ from tabarena.benchmark.models.ag.ebm.ebm_model import ExplainableBoostingMachin
 from tabarena.benchmark.models.ag.knn_new.knn_model import KNNNewModel
 from tabarena.benchmark.models.ag.modernnca.modernnca_model import ModernNCAModel
 from tabarena.benchmark.models.ag.realmlp.realmlp_model import RealMLPModel
+from tabarena.benchmark.models.ag.sap_rpt_oss.sap_rpt_oss_model import SAPRPTOSSModel
 from tabarena.benchmark.models.ag.tabdpt.tabdpt_model import TabDPTModel
 from tabarena.benchmark.models.ag.tabicl.tabicl_model import TabICLModel
 from tabarena.benchmark.models.ag.tabm.tabm_model import TabMModel
@@ -16,6 +17,7 @@ __all__ = [
     "ModernNCAModel",
     "RealMLPModel",
     "RealTabPFNv25Model",
+    "SAPRPTOSSModel",
     "TabDPTModel",
     "TabICLModel",
     "TabMModel",
