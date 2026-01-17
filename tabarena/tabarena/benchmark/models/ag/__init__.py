b1b55852a17a32ea9a937508ad2d6d72ba078542
LennartPurucker <contact@lennart-purucker.com>
2026-01-17 16:35:38 +0100

    maint/add: see change log in grande_model

--- tabarena/tabarena/benchmark/models/ag/__init__.py
+++ tabarena/tabarena/benchmark/models/ag/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from tabarena.benchmark.models.ag.ebm.ebm_model import ExplainableBoostingMachineModel
+from tabarena.benchmark.models.ag.grande.grande_model import GRANDEModel
 from tabarena.benchmark.models.ag.knn_new.knn_model import KNNNewModel
 from tabarena.benchmark.models.ag.modernnca.modernnca_model import ModernNCAModel
 from tabarena.benchmark.models.ag.realmlp.realmlp_model import RealMLPModel
@@ -10,10 +11,10 @@ from tabarena.benchmark.models.ag.tabicl.tabicl_model import TabICLModel
 from tabarena.benchmark.models.ag.tabm.tabm_model import TabMModel
 from tabarena.benchmark.models.ag.tabpfnv2_5.tabpfnv2_5_model import RealTabPFNv25Model
 from tabarena.benchmark.models.ag.xrfm.xrfm_model import XRFMModel
-from tabrepo.benchmark.models.ag.grande.grande_model import GRANDEModel
 
 __all__ = [
     "ExplainableBoostingMachineModel",
+    "GRANDEModel",
     "KNNNewModel",
     "ModernNCAModel",
     "RealMLPModel",
@@ -23,5 +24,4 @@ __all__ = [
     "TabICLModel",
     "TabMModel",
     "XRFMModel",
-    "GRANDEModel",
 ]
--- tabarena/tabarena/models/grande/generate.py
+++ tabarena/tabarena/models/grande/generate.py
@@ -2,50 +2,49 @@ from __future__ import annotations
 
 import numpy as np
 
-from tabrepo.benchmark.experiment import YamlExperimentSerializer
-from tabrepo.benchmark.models.ag.grande.grande_model import GRANDEModel
-from tabrepo.models.utils import convert_numpy_dtypes
-from tabrepo.utils.config_utils import CustomAGConfigGenerator, generate_bag_experiments
+from tabarena.benchmark.experiment import YamlExperimentSerializer
+from tabarena.benchmark.models.ag.grande.grande_model import GRANDEModel
+from tabarena.models.utils import convert_numpy_dtypes
+from tabarena.utils.config_utils import (
+    CustomAGConfigGenerator,
+    generate_bag_experiments,
+)
+
 
 def generate_single_config_grande(rng):
     # common search space
     params = {
-                'depth': rng.choice([3, 5], p=[0.5, 0.5]),
-                'n_estimators': 1024,
-                'learning_rate_weights': 0.001, #np.exp(rng.uniform(np.log(1e-4), np.log(1e-1))),
-                'learning_rate_index': 0.01, #np.exp(rng.uniform(np.log(5e-4), np.log(1e-1))),
-                'learning_rate_values': np.exp(rng.uniform(np.log(1e-3), np.log(5e-2))),
-                'learning_rate_leaf': np.exp(rng.uniform(np.log(1e-2), np.log(5e-1))),
-                'temperature': 0.0,
-                'use_class_weights': False, #rng.choice([True, False], p=[0.5, 0.5]), #False,
-                'dropout': rng.choice([0.0, 0.2]),
-                'selected_variables': 0.8, #rng.choice([0.5, 0.8], p=[0.5, 0.5]),
-                'data_subset_fraction': 1.0, #rng.choice([0.5, 0.8, 1.0], p=[0.25, 0.25, 0.5]),#1.0, #rng.choice([0.5, 0.8, 1.0], p=[0.25, 0.25, 0.5]),
-                'bootstrap': False, #rng.choice([True, False], p=[0.5, 0.5]),
-                'random_seed': 123,
-                'verbose': 0,
-
-                'batch_size': 256,
-                'early_stopping_epochs': 50,
-                'epochs': 250,
-                'focal_loss': False, #rng.choice([True, False], p=[0.5, 0.5]),
-                'es_metric': True,
-                'missing_values': False,
-                'swa': False,
-                'cosine_decay_restarts': rng.choice([True, False], p=[0.5, 0.5]), #
-                'optimizer': "adam", 
-                'reduce_on_plateau_scheduler': True,
-
-                'learning_rate_embedding': np.exp(rng.uniform(np.log(1e-4), np.log(5e-2))),
-                'use_category_embeddings': rng.choice([True, False], p=[0.5, 0.5]),
-                'embedding_dim_cat': 8,
-                'use_numeric_embeddings': rng.choice([True, False], p=[0.5, 0.5]),
-                'embedding_dim_num': 8,
-                'embedding_threshold': 1,
-
-                'label_smoothing': 0.00, #rng.choice([0.00, 0.1], p=[0.5, 0.5]), #rng.choice([0.00, 0.1], p=[0.5, 0.5]),
-
-                'use_robust_scale_smoothing': False,
+        "depth": rng.choice([3, 5], p=[0.5, 0.5]),
+        "n_estimators": 1024,
+        "learning_rate_weights": 0.001,  # np.exp(rng.uniform(np.log(1e-4), np.log(1e-1))),
+        "learning_rate_index": 0.01,  # np.exp(rng.uniform(np.log(5e-4), np.log(1e-1))),
+        "learning_rate_values": np.exp(rng.uniform(np.log(1e-3), np.log(5e-2))),
+        "learning_rate_leaf": np.exp(rng.uniform(np.log(1e-2), np.log(5e-1))),
+        "temperature": 0.0,
+        "use_class_weights": False,  # rng.choice([True, False], p=[0.5, 0.5]), #False,
+        "dropout": rng.choice([0.0, 0.2]),
+        "selected_variables": 0.8,  # rng.choice([0.5, 0.8], p=[0.5, 0.5]),
+        "data_subset_fraction": 1.0,  # rng.choice([0.5, 0.8, 1.0], p=[0.25, 0.25, 0.5]),#1.0, #rng.choice([0.5, 0.8, 1.0], p=[0.25, 0.25, 0.5]),
+        "bootstrap": False,  # rng.choice([True, False], p=[0.5, 0.5]),
+        "verbose": 0,
+        "batch_size": 256,
+        "early_stopping_epochs": 50,
+        "epochs": 250,
+        "focal_loss": False,  # rng.choice([True, False], p=[0.5, 0.5]),
+        "es_metric": True,
+        "missing_values": False,
+        "swa": False,
+        "cosine_decay_restarts": rng.choice([True, False], p=[0.5, 0.5]),
+        "optimizer": "adam",
+        "reduce_on_plateau_scheduler": True,
+        "learning_rate_embedding": np.exp(rng.uniform(np.log(1e-4), np.log(5e-2))),
+        "use_category_embeddings": rng.choice([True, False], p=[0.5, 0.5]),
+        "embedding_dim_cat": 8,
+        "use_numeric_embeddings": rng.choice([True, False], p=[0.5, 0.5]),
+        "embedding_dim_num": 8,
+        "embedding_threshold": 1,
+        "label_smoothing": 0.00,  # rng.choice([0.00, 0.1], p=[0.5, 0.5]), #rng.choice([0.00, 0.1], p=[0.5, 0.5]),
+        "use_robust_scale_smoothing": False,
     }
 
     return convert_numpy_dtypes(params)
@@ -76,6 +75,10 @@ if __name__ == "__main__":
         time_limit=3600,
         name_id_prefix="c",
     )
-    experiments_random = generate_bag_experiments(model_cls=GRANDEModel, configs=configs, time_limit=3600)
+    experiments_random = generate_bag_experiments(
+        model_cls=GRANDEModel, configs=configs, time_limit=3600
+    )
     experiments = experiments_default + experiments_random
-    YamlExperimentSerializer.to_yaml(experiments=experiments, path="configs_grande.yaml")
+    YamlExperimentSerializer.to_yaml(
+        experiments=experiments, path="configs_grande.yaml"
+    )
