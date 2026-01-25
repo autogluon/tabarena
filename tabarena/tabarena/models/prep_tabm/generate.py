713f2ec867b16a95d9236352a6f5afd2cad4dd47
atschalz <andrej.tschalzev@uni-mannheim.de>
2026-01-25 17:47:07 +0100

    Update TabM search space

--- tabarena/tabarena/models/prep_tabm/generate.py
+++ tabarena/tabarena/models/prep_tabm/generate.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import numpy as np
-from autogluon.common.space import Categorical
+# from autogluon.common.space import Categorical
 from tabarena.benchmark.models.prep_ag.prep_tabm.prep_tabm_model import PrepTabMModel
 from tabarena.models.utils import convert_numpy_dtypes
 from ...utils.config_utils import PrepConfigGenerator
+from ConfigSpace import Categorical, ConfigurationSpace, Float, Integer
+from tabarena.benchmark.experiment import YamlExperimentSerializer
+from tabarena.utils.config_utils import CustomAGConfigGenerator
 
-name = "TabM"
+name = "prep_TabM"
 
 prep_manual_configs = [
     {
@@ -17,29 +20,29 @@ prep_manual_configs = [
         "use_select_spearman": True,
     }]
 
-prep_search_space = {
-        # Preprocessing hyperparameters
-        "use_arithmetic_preprocessor": Categorical(True),
-        "use_cat_fe": Categorical(True),
-        "use_rstafc": Categorical(True),
-        "use_groupby": Categorical(True), 
-        "use_select_spearman": Categorical(True), # Might rather tune no. of features, i.e. in {1000, 1500, 2000}
+# prep_search_space = {
+#         # Preprocessing hyperparameters
+#         "use_arithmetic_preprocessor": Categorical(True),
+#         "use_cat_fe": Categorical(True),
+#         "use_rstafc": Categorical(True),
+#         "use_groupby": Categorical(True), 
+#         "use_select_spearman": Categorical(True), # Might rather tune no. of features, i.e. in {1000, 1500, 2000}
 
-        "arithmetic_max_feats": Categorical(2000, 1000),
-        "arithmetic_random_state": Categorical(42,84,168,336,672),
+#         "arithmetic_max_feats": Categorical(2000, 1000),
+#         "arithmetic_random_state": Categorical(42,84,168,336,672),
 
-        "cat_fe_max_feats": Categorical(100, 500),
-        "cat_fe_random_state": Categorical(42,84,168,336,672),
+#         "cat_fe_max_feats": Categorical(100, 500),
+#         "cat_fe_random_state": Categorical(42,84,168,336,672),
 
-        "rstafc_n_subsets": Categorical(50,100, 1),
-        "rstafc_random_state": Categorical(42,84,168,336,672),
+#         "rstafc_n_subsets": Categorical(50,100, 1),
+#         "rstafc_random_state": Categorical(42,84,168,336,672),
 
-        "oofte_random_state": Categorical(42,84,168,336,672),
+#         "oofte_random_state": Categorical(42,84,168,336,672),
 
-        "groupby_max_feats": Categorical(500, 100, 1000), 
+#         "groupby_max_feats": Categorical(500, 100, 1000), 
 
-        "spearman_max_feats": Categorical(2000),
-}       
+#         "spearman_max_feats": Categorical(2000),
+# }       
 
 def generate_single_config_tabm(rng):
     # taken from https://github.com/yandex-research/tabm/blob/main/exp/tabm-mini-piecewiselinear/adult/0-tuning.toml
@@ -76,13 +79,143 @@ def generate_single_config_tabm(rng):
 def generate_configs_tabm(num_random_configs=200, seed=1234):
     # note: this doesn't set val_metric_name, which should be set outside
     rng = np.random.default_rng(seed)
-    return [generate_single_config_tabm(rng) for _ in range(num_random_configs)]
+    configs = [generate_single_config_tabm(rng) for _ in range(num_random_configs)]
 
 
-gen_tabm = PrepConfigGenerator(
-    model_cls=PrepTabMModel, 
+    prep_search_space = ConfigurationSpace(
+        space=[
+            # Preprocessing hyperparameters
+            Categorical("use_arithmetic_preprocessor", [True]),
+            Categorical("use_cat_fe", [True]),
+            Categorical("use_rstafc", [True]),
+            Categorical("use_groupby", [True]), 
+            Categorical("use_select_spearman", [True]), # Might rather tune no. of features, i.e. in {1000, 1500, 2000}
+
+            Categorical("arithmetic_max_feats", [2000, 1000]),
+            Categorical("arithmetic_random_state", [42,84,168,336,672]),
+
+            Categorical("cat_fe_max_feats", [100, 500]),
+            Categorical("cat_fe_random_state", [42,84,168,336,672]),
+
+            Categorical("rstafc_n_subsets", [50,100, 1]),
+            Categorical("rstafc_random_state", [42,84,168,336,672]),
+
+            Categorical("oofte_random_state", [42,84,168,336,672]),
+
+            Categorical("groupby_max_feats", [500, 100, 1000]), 
+
+            Categorical("spearman_max_feats", [2000]),
+
+        ],
+        seed=123,
+    )       
+
+    prep_configs = prep_search_space.sample_configuration(num_random_configs)
+    
+    if num_random_configs == 1:
+        configs = [configs]
+        prep_configs = [prep_configs]
+    configs = [dict(config) for config in configs]
+    prep_configs = [dict(config) for config in prep_configs]
+
+    for i in range(len(prep_configs)):
+        if 'ag.prep_params' not in configs[i]:
+            configs[i]['ag.prep_params'] = []
+        pipeline = []
+        prep_params_passthrough_types = None
+        use_arithmetic_preprocessor = prep_configs[i].pop('use_arithmetic_preprocessor', False)
+        use_cat_fe = prep_configs[i].pop('use_cat_fe', False)
+        use_tafc = prep_configs[i].pop('use_tafc', False)
+        use_rstafc = prep_configs[i].pop('use_rstafc', False)
+        use_neighbor_interactions = prep_configs[i].pop('use_neighbor_interactions', False)
+        use_neighbor_structure = prep_configs[i].pop('use_neighbor_structure', False)
+        use_groupby = prep_configs[i].pop('use_groupby', False)
+        use_linear_feature = prep_configs[i].pop('use_linear_feature', False)
+        use_select_spearman = prep_configs[i].pop('use_select_spearman', False)
+        arithmetic_max_feats = prep_configs[i].pop('arithmetic_max_feats', 2000)
+        arithmetic_random_state = prep_configs[i].pop('arithmetic_max_feats_random_state', 42)
+        cat_fe_max_feats = prep_configs[i].pop('cat_fe_max_feats', 100)
+        cat_fe_random_state = prep_configs[i].pop('cat_fe_random_state', 42)
+        rstafc_n_subsets = prep_configs[i].pop('rstafc_n_subsets', 50)
+        rstafc_random_state = prep_configs[i].pop('rstafc_random_state', 42)
+        oofte_random_state = prep_configs[i].pop('oofte_random_state', 42)
+        groupby_max_feats = prep_configs[i].pop('groupby_max_feats', 500)
+        spearman_max_feats = prep_configs[i].pop('spearman_max_feats', 2000)
+        
+
+        if use_groupby:
+            pipeline.append(['GroupByFeatureGenerator', {"max_features": groupby_max_feats}])
+            
+        if use_tafc:
+            pipeline.append(['TargetAwareFeatureCompressionFeatureGenerator', {"random_state": oofte_random_state}])
+
+        if use_rstafc:
+            pipeline.append(['RandomSubsetTAFC', {"n_subsets": rstafc_n_subsets, "random_state": rstafc_random_state}])
+
+        if use_neighbor_interactions:
+            pipeline.append(['NeighborInteractionFeatureGenerator', {}])
+        
+        if use_neighbor_structure:
+            pipeline.append(['NeighborStructureFeatureGenerator', {}])
+
+        if use_linear_feature:
+            pipeline.append(['LinearFeatureGenerator', {}])
+        
+        if use_arithmetic_preprocessor:
+            _generator_params = {"max_new_feats": arithmetic_max_feats, "random_state": arithmetic_random_state}
+            pipeline.append(['ArithmeticFeatureGenerator', _generator_params])
+
+        cat_pipeline = [['OOFTargetEncodingFeatureGenerator', {}]]
+        prep_params_passthrough_types = {"invalid_raw_types": ["category", "object"]}
+        if use_cat_fe:
+            cat_pipeline.append([
+                ['CategoricalInteractionFeatureGenerator', {"passthrough": True, "max_new_feats": cat_fe_max_feats, "random_state": cat_fe_random_state}],
+            ])
+            cat_pipeline.reverse()#
+        pipeline.append(cat_pipeline)
+
+        if use_select_spearman:
+            configs[i]['ag.prep_params'].append(pipeline)
+            configs[i]['ag.prep_params'].append([
+                ['SpearmanFeatureSelector', {'max_features': spearman_max_feats}],
+            ])
+        else:
+            configs[i]['ag.prep_params'].extend(pipeline)
+
+
+        if prep_params_passthrough_types:
+            configs[i]['ag.prep_params.passthrough_types'] = prep_params_passthrough_types
+
+
+    return configs
+
+gen_tabm = CustomAGConfigGenerator(
+    model_cls=PrepTabMModel,
     search_space_func=generate_configs_tabm,
-    prep_manual_configs=prep_manual_configs,
-    prep_search_space=prep_search_space
-                             )
+    manual_configs=[
+        {
+        'ag.prep_params': [
+            [
+                ['GroupByFeatureGenerator', {}],
+                ['RandomSubsetTAFC', {}],
+                ['ArithmeticFeatureGenerator', {}],
+                [
+                    ['CategoricalInteractionFeatureGenerator', {"passthrough": True}],
+                    ['OOFTargetEncodingFeatureGenerator', {}],
+                ]],            
+        ['SpearmanFeatureSelector', {'max_features': 2000}]            
+        ],
+        'ag.prep_params.passthrough_types': {"invalid_raw_types": ["category", "object"]}, # We never keep categorical features
+        'ag.use_residuals': False,
+        'ag.residual_type': 'oof',
+        'ag.max_dataset_size_for_residuals': 1000,
+        'ag.residual_init_kwargs': {},
+        },
+    ],
+)
 
+if __name__ == "__main__":
+    experiments = gen_tabm.generate_all_bag_experiments(num_random_configs=200)
+    YamlExperimentSerializer.to_yaml(
+        experiments=experiments, path="configs_prep_tabm_alt.yaml"
+    )
