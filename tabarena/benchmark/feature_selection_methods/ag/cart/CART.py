03726c5ec5171ffb3b12715bbe811edfe0cde5b4
bastis007 <schaefer.bastian@mail.de>
2026-02-25 13:53:04 +0100

    CART sklearn implementation, corresponds to the literature, I think it's legit

--- tabarena/benchmark/feature_selection_methods/ag/cart/CART.py
+++ tabarena/benchmark/feature_selection_methods/ag/cart/CART.py
@@ -25,11 +25,12 @@ class CART(AbstractFeatureSelector):
         self._model = model
         self._n_max_features = n_max_features
         from tabarena.benchmark.feature_selection_methods.ag.cart.method.CARTFS import CARTFS
-        self._cart = CARTFS(model)
+        self._cart = CARTFS()
         # Time limit
         if "time_limit" in kwargs and kwargs["time_limit"] is not None:
             time_start_fit = time.time()
             kwargs["time_limit"] -= time_start_fit - kwargs["start_time"]
+            kwargs["start_time"] = time_start_fit
             if kwargs["time_limit"] <= 0:
                 logger.warning(
                     f'\tWarning: FeatureSelection Method has no time left to train... (Time Left = {kwargs["time_limit"]:.1f}s)')
--- tabarena/benchmark/feature_selection_methods/ag/cart/method/CARTFS.py
+++ tabarena/benchmark/feature_selection_methods/ag/cart/method/CARTFS.py
@@ -4,18 +4,27 @@ import numpy as np
 import pandas as pd
 
 import warnings
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+import copy
 import logging
+import time
+
+from sklearn.tree import DecisionTreeClassifier
 
 logger = logging.getLogger(__name__)
+
 warnings.filterwarnings('ignore')
 
 
 class CARTFS:
     """CART feature selector"""
 
-    def __init__(self, model):
+    def __init__(self):
         self._y = None
-        self._model = model
+        self._model = None
         self._n_max_features = None
         self._selected_features = None
 
@@ -23,14 +32,12 @@ class CARTFS:
         self._y = y
         self._model = model
         self._n_max_features = n_max_features
-        X_np = X.to_numpy()
-        y_np = y.to_numpy()
 
-        feature_ranking = self.feature_ranking(self.t_score(X_np, y_np))
+        feature_ranking = self.cart(X, y)
 
-        selected_features_idx = feature_ranking[:n_max_features]
+        sorted_idx = np.argsort(-feature_ranking)
+        selected_features_idx = sorted_idx[:n_max_features]
         selected_features = X.columns[selected_features_idx]
-
         X_selected = X[selected_features]
         self._selected_features = list(X_selected.columns)
         return X_selected
@@ -41,52 +48,10 @@ class CARTFS:
             self.fit_transform(X, self._y, self._model, self._n_max_features)
         return X[self._selected_features]
 
-    def t_score(self, X, y):
-        """
-        This function calculates t_score for each feature, where t_score is only used for binary problem
-        t_score = |mean1-mean2|/sqrt(((std1^2)/n1)+((std2^2)/n2)))
 
-        Input
-        -----
-        X: {numpy array}, shape (n_samples, n_features)
-            input data
-        y: {numpy array}, shape (n_samples,)
-            input class labels
-
-        Output
-        ------
-        F: {numpy array}, shape (n_features,)
-            t-score for each feature
-        """
-
-        n_samples, n_features = X.shape
-        F = np.zeros(n_features)
-        c = np.unique(y)
-        if len(c) == 2:
-            for i in range(n_features):
-                f = X[:, i]
-                # class0 contains instances belonging to the first class
-                # class1 contains instances belonging to the second class
-                class0 = f[y == c[0]]
-                class1 = f[y == c[1]]
-                mean0 = np.mean(class0)
-                mean1 = np.mean(class1)
-                std0 = np.std(class0)
-                std1 = np.std(class1)
-                n0 = len(class0)
-                n1 = len(class1)
-                t = mean0 - mean1
-                t0 = np.true_divide(std0 ** 2, n0)
-                t1 = np.true_divide(std1 ** 2, n1)
-                F[i] = np.true_divide(t, (t0 + t1) ** 0.5)
-        else:
-            print('y should be guaranteed to a binary class vector')
-            exit(0)
-        return np.abs(F)
-
-    def feature_ranking(self, F):
-        """
-        Rank features in descending order according to t-score, the higher the t-score, the more important the feature is
-        """
-        idx = np.argsort(F)
-        return idx[::-1]
+    def cart(self, X_train, y_train):
+        CART = DecisionTreeClassifier(random_state=0)
+        CART.fit(X_train, y_train)
+        importances = CART.feature_importances_
+        # std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+        return importances
