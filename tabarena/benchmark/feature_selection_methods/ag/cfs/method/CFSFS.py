24ec6384dffd1817e3919117dfadf2fbe784ef16
bastis007 <schaefer.bastian@mail.de>
2026-03-04 13:40:53 +0100

    Adapting time limit for CFS (the version using symmetrical uncertainty), implementation corresponds to the literature (https://github.com/ZixiaoShen/Correlation-based-Feature-Selection)

--- tabarena/benchmark/feature_selection_methods/ag/cfs/method/CFSFS.py
+++ tabarena/benchmark/feature_selection_methods/ag/cfs/method/CFSFS.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from math import log
+
 import numpy as np
 import pandas as pd
 
@@ -11,7 +13,14 @@ warnings.filterwarnings('ignore')
 
 
 class CFSFS:
-    """CFS feature selector"""
+    """CFS feature selector
+
+    There exist the following variation: CFS-UC uses symmetrical uncertainty to measure
+    correlations, CFS-MDL uses normalized symmetrical MDL to measure correlations, and
+    CFS-Relief uses symmetrical relief to measure correlations
+
+    Here, we used the CFS-UC implementation from https://github.com/ZixiaoShen/Correlation-based-Feature-Selection
+    """
 
     def __init__(self, model):
         self._y = None
@@ -26,7 +35,7 @@ class CFSFS:
         X_np = X.to_numpy()
         y_np = y.to_numpy()
 
-        feature_ranking = self.feature_ranking(self.t_score(X_np, y_np))
+        feature_ranking = self.cfs(X_np, y_np)
 
         selected_features_idx = feature_ranking[:n_max_features]
         selected_features = X.columns[selected_features_idx]
@@ -41,52 +50,135 @@ class CFSFS:
             self.fit_transform(X, self._y, self._model, self._n_max_features)
         return X[self._selected_features]
 
-    def t_score(self, X, y):
+    def cfs(self, X, y):
         """
-        This function calculates t_score for each feature, where t_score is only used for binary problem
-        t_score = |mean1-mean2|/sqrt(((std1^2)/n1)+((std2^2)/n2)))
+        This function uses a correlation based heuristic to evaluate the worth of features which is called CFS
 
-        Input
-        -----
-        X: {numpy array}, shape (n_samples, n_features)
-            input data
-        y: {numpy array}, shape (n_samples,)
-            input class labels
-
-        Output
-        ------
-        F: {numpy array}, shape (n_features,)
-            t-score for each feature
+        :param X: {numpy array}, shape (n_samples, n_features) input data
+        :param y: {numpy array}, shape (n_samples) input class labels
+        :return F: {numpy array}, index of selected features
         """
 
         n_samples, n_features = X.shape
-        F = np.zeros(n_features)
-        c = np.unique(y)
-        if len(c) == 2:
+        F = []
+        M = []  # M stores the merit values
+        while True:
+            merit = -100000000000
+            idx = -1
             for i in range(n_features):
-                f = X[:, i]
-                # class0 contains instances belonging to the first class
-                # class1 contains instances belonging to the second class
-                class0 = f[y == c[0]]
-                class1 = f[y == c[1]]
-                mean0 = np.mean(class0)
-                mean1 = np.mean(class1)
-                std0 = np.std(class0)
-                std1 = np.std(class1)
-                n0 = len(class0)
-                n1 = len(class1)
-                t = mean0 - mean1
-                t0 = np.true_divide(std0 ** 2, n0)
-                t1 = np.true_divide(std1 ** 2, n1)
-                F[i] = np.true_divide(t, (t0 + t1) ** 0.5)
-        else:
-            print('y should be guaranteed to a binary class vector')
-            exit(0)
-        return np.abs(F)
+                if i not in F:
+                    F.append(i)
+                    # calculate the merit of current selected features
+                    t = self.merit_calculation(X[:, F], y)
+                    if t > merit:
+                        merit = t
+                        idx = i
+                    F.pop()
+            F.append(idx)
+            M.append(merit)
+            if len(M) > 5:
+                if M[len(M) - 1] <= M[len(M) - 2]:
+                    if M[len(M) - 2] <= M[len(M) - 3]:
+                        if M[len(M) - 3] <= M[len(M) - 4]:
+                            if M[len(M) - 4] <= M[len(M) - 5]:
+                                break
+        return np.array(F)
 
-    def feature_ranking(self, F):
+    def merit_calculation(self, X, y):
         """
-        Rank features in descending order according to t-score, the higher the t-score, the more important the feature is
+        This function calculates the merit of X given class labels y, where
+        merits = (k * rcf) / sqrt (k + k*(k-1)*rff)
+        rcf = (1/k)*sum(su(fi, y)) for all fi in X
+        rff = (1/(k*(k-1)))*sum(su(fi, fj)) for all fi and fj in X
+
+        :param X:  {numpy array}, shape (n_samples, n_features) input data
+        :param y:  {numpy array}, shape (n_samples) input class labels
+        :return merits: {float}  merit of a feature subset X
         """
-        idx = np.argsort(F)
-        return idx[::-1]
+
+        n_samples, n_features = X.shape
+        rff = 0
+        rcf = 0
+        for i in range(n_features):
+            fi = X[:, i]
+            rcf += self.su_calculation(fi, y)  # su is the symmetrical uncertainty of fi and y
+            for j in range(n_features):
+                if j > i:
+                    fj = X[:, j]
+                    rff += self.su_calculation(fi, fj)
+        rff *= 2
+        merits = rcf / np.sqrt(n_features + rff)
+        return merits
+
+    def information_gain(self, f1, f2):
+        """
+        This function calculates the information gain, where ig(f1, f2) = H(f1) - H(f1\f2)
+
+        :param f1: {numpy array}, shape (n_samples,)
+        :param f2: {numpy array}, shape (n_samples,)
+        :return: ig: {float}
+        """
+
+        ig = self.entropyd(f1) - self.conditional_entropy(f1, f2)
+        return ig
+
+    def conditional_entropy(self, f1, f2):
+        """
+        This function calculates the conditional entropy, where ce = H(f1) - I(f1;f2)
+        :param f1: {numpy array}, shape (n_samples,)
+        :param f2: {numpy array}, shape (n_samples,)
+        :return: ce {float} conditional entropy of f1 and f2
+        """
+
+        ce = self.entropyd(f1) - self.midd(f1, f2)
+        return ce
+
+    def su_calculation(self, f1, f2):
+        """
+        This function calculates the symmetrical uncertainty, where su(f1,f2) = 2*IG(f1,f2)/(H(f1)+H(f2))
+        :param f1: {numpy array}, shape (n_samples,)
+        :param f2: {numpy array}, shape (n_samples,)
+        :return: su {float} su is the symmetrical uncertainty of f1 and f2
+        """
+        # calculate information gain of f1 and f2, t1 = ig(f1, f2)
+        t1 = self.information_gain(f1, f2)
+        # calculate entropy of f1
+        t2 = self.entropyd(f1)
+        # calculate entropy of f2
+        t3 = self.entropyd(f2)
+
+        su = 2.0 * t1 / (t2 + t3)
+
+        return su
+
+    def entropyd(self, sx, base=2):
+        """
+        Discrete entropy estimator given a list of samples which can be any hashable object
+        """
+
+        return self.entropyfromprobs(self.hist(sx), base=base)
+
+    def hist(self, sx):
+        # Histogram from list of samples
+        d = dict()
+        for s in sx:
+            d[s] = d.get(s, 0) + 1
+        return map(lambda z: float(z) / len(sx), d.values())
+
+    def entropyfromprobs(self, probs, base=2):
+        # Turn a normalized list of probabilities of discrete outcomes into entropy (base 2)
+        return -sum(map(self.elog, probs)) / log(base)
+
+    def elog(self, x):
+        # for entropy, 0 log 0 = 0. but we get an error for putting log 0
+        if x <= 0. or x >= 1.:
+            return 0
+        else:
+            return x * log(x)
+
+    def midd(self, x, y):
+        """
+        Discrete mutual information estimator given a list of samples which can be any hashable object
+        """
+
+        return -self.entropyd(list(zip(x, y))) + self.entropyd(x) + self.entropyd(y)
