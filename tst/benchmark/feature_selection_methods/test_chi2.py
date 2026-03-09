3bd815b2ca7cc3c3d55d6fe0de0e67b616e1a160
Lennart Purucker <contact@lennart-purucker.com>
2026-03-09 12:18:02 +0100

    Maint Rebase FE Benchmark (#263)
    Co-authored-by: bastis007 <schaefer.bastian@mail.de>

---
+++ tst/benchmark/feature_selection_methods/test_chi2.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import pytest
+
+from tst.benchmark.feature_selection_methods.fs_test_utils import verify_method
+
+
+@pytest.mark.parametrize(
+    ("problem_type", "dataset_id"),
+    [
+        ("binary", 55),
+        ("multiclass", 10),
+        ("regression", 46964),
+    ],
+)
+def test_chi2(problem_type, dataset_id):
+    from tabarena.benchmark.feature_selection_methods.ag.chi2.Chi2 import Chi2
+
+    hyperparameters = {"n_max_features": 10, "time_limit": 3600, "dataset_id": dataset_id, "problem_type": problem_type}
+    try:
+        verify_method(Chi2, hyperparameters)
+    except ImportError as err:
+        pytest.skip(
+            f"Import Error, skipping test... Ensure you have the proper dependencies installed to run this test:\n{err}"
+        )
