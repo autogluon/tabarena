d901f5f0210bf4c8bced4db2832a95810f5e067a
bastis007 <schaefer.bastian@mail.de>
2026-03-16 14:10:32 +0100

    add: relief_f

--- experimental/feature_selection_benchmark/run_autogluon_feature_selection_pipeline.py
+++ experimental/feature_selection_benchmark/run_autogluon_feature_selection_pipeline.py
@@ -401,6 +401,7 @@ def run_example():
     from experimental.feature_selection_benchmark.mrmr.mrmr import mRMRFeatureSelector
     from experimental.feature_selection_benchmark.one_r.one_r import OneRFeatureSelector
     from experimental.feature_selection_benchmark.pearson_correlation.pearson_correlation import PearsonCorrelationFeatureSelector
+    from experimental.feature_selection_benchmark.relief_f.relief_f import ReliefFFeatureSelector
     from experimental.feature_selection_benchmark.symmetrical_uncertainty.symmetrical_uncertainty import SymmetricalUncertaintyFeatureSelector
 
     for feature_selector in [
@@ -426,6 +427,7 @@ def run_example():
         mRMRFeatureSelector(max_features=max_features),
         OneRFeatureSelector(max_features=max_features),
         PearsonCorrelationFeatureSelector(max_features=max_features),
+        ReliefFFeatureSelector(max_features=max_features),
         SymmetricalUncertaintyFeatureSelector(max_features=max_features),
     ]:
         print("\n####### Running feature selector:", feature_selector.name)
