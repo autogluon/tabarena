3026f2aa998ed3f47c39ac767b2cdcb4d67b7763
bastis007 <schaefer.bastian@mail.de>
2026-03-16 11:58:29 +0100

    add: pearson_correlation

---
+++ experimental/feature_selection_benchmark/pearson_correlation/pearson_correlation.py
@@ -0,0 +1,30 @@
+import logging
+
+import pandas as pd
+from sklearn.feature_selection import SelectKBest, r_regression
+
+from experimental.feature_selection_benchmark.run_autogluon_feature_selection_pipeline import AbstractFeatureSelector
+
+logger = logging.getLogger(__name__)
+
+
+class PearsonCorrelationFeatureSelector(AbstractFeatureSelector):
+    """
+    PearsonCorrelation Feature Selection.
+
+    Reference: Liu, Huan, and Rudy Setiono. "Chi2: Feature selection and discretization of numeric attributes." Proceedings of 7th IEEE international conference on tools with artificial intelligence. Ieee, 1995.
+    Implementation Source: https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.chi2.html
+    """
+
+
+
+    name = "PearsonCorrelationFeatureSelector"
+    feature_scoring_method: bool = True
+
+    def _fit_feature_scoring(self, *, X: pd.DataFrame, y: pd.Series, time_limit: int | None = None) -> dict[str, float]:
+        pc_kwargs = {"score_func": r_regression, "k": "all"}
+        pcFS = SelectKBest(**pc_kwargs)
+        pcFS.fit(X, y)
+        scores = pcFS.scores_
+        feature_scores = dict(zip(X.columns, scores))
+        return feature_scores
