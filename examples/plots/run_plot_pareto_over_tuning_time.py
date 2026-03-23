b716ebf359001892c8c5ee3c1a607223af2a992d
Nick Erickson <innixma@gmail.com>
2026-03-23 03:18:51 +0000

    fix method filter after display_name update

--- examples/plots/run_plot_pareto_over_tuning_time.py
+++ examples/plots/run_plot_pareto_over_tuning_time.py
@@ -526,7 +526,7 @@ def plot_tuning_trajectories(
         leaderboard["name"] = leaderboard["name"].map(method_rename_map).fillna(leaderboard["name"])
 
         if ban_bad_methods:
-            bad_methods = ["KNN", "LR"]
+            bad_methods = ["KNN", "Linear"]
             leaderboard = leaderboard[~leaderboard["config_type"].isin(bad_methods)]
 
         fig_save_dir_subset = fig_save_dir / subset_name
