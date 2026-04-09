d923ac8f5ece8828d8eb3ecb02f3eaff0f9777d6
domiomi3 <matusd@informatik.uni-freiburg.de>
2026-04-09 12:13:57 +0200

    add: setup for dominika

---
+++ experimental/feature_selection_benchmark/tabarena_setup/run_setup_slurm_jobs_fe_benchmark_dominika.py
@@ -0,0 +1,72 @@
+"""Run TabArena for feature selection benchmark with downstream model performance evaluation."""
+from __future__ import annotations
+
+import os
+import sys
+
+paths = [
+    "/work/dlclarge1/purucker-fs_benchmark/code/fsbench_matusd/tabarena/experimental/feature_selection_benchmark/tabarena_setup",
+    "/work/dlclarge1/purucker-fs_benchmark/code/fsbench_matusd/tabarena",
+    "/work/dlclarge1/purucker-fs_benchmark/code/fsbench_matusd/tabarena/tabflow_slurm"
+]
+
+# For current script
+for path in paths:
+    sys.path.insert(0, path)
+
+# For child processes
+existing = os.environ.get('PYTHONPATH', '')
+os.environ['PYTHONPATH'] = ':'.join(paths + ([existing] if existing else []))
+
+from fr_cluster_setup import ALL_TASK_METADATA, FSBenchmarkConfig, TabArenaBenchmarkSetup, UniPathSetupDominika
+
+preprocessing_pipelines = FSBenchmarkConfig().get_default_preprocessing_configs(
+    fs_methods=[
+        "AccuracyFeatureSelector",
+        "RandomFeatureSelector",
+        "ANOVAFeatureSelector",
+        "CFSFeatureSelector",
+        "Chi2FeatureSelector",
+        "DISRFeatureSelector",
+        "GainRatioFeatureSelector",
+        "GiniFeatureSelector",
+        "ImpurityFeatureSelector",
+        "InformationGainFeatureSelector",
+        "INTERACTFeatureSelector",
+        "MarkovBlanketFeatureSelector",
+        "MIFeatureSelector",
+        "mRMRFeatureSelector",
+        "PearsonCorrelationFeatureSelector",
+        "ReliefFFeatureSelector",
+        "RFImportanceFeatureSelector",
+        "SequentialBackwardEliminationFeatureSelector",
+        "SequentialForwardSelectionFeatureSelector",
+        "SymmetricalUncertaintyFeatureSelector",
+        # "LassoFeatureSelector", # just for regression but with label encoder for classification?
+        # "LaplacianScoreFeatureSelector", # OOM, Segmentation fault issues
+        # "ConsistencyFeatureSelector", # selected_indices = np.where(S)[0].tolist(), UnboundLocalError: cannot access local variable 'S' where it is not associated with a value
+        # "JMIFeatureSelector", # time limit computed incorrectly, and error at remaining.remove(best_idx), ValueError: list.remove(x): x not in list
+        # "OneRFeatureSelector", # major OOM errors (tries to allocate one major array), wrong time limit computation,  max(accuracies, key=accuracies.get) -> max() iterable argument is empty
+        # "ElasticNetFeatureSelector", # Only for classification
+        # "CMIMFeatureSelector", # problems with time limit and fallback of features
+        # "tTestFeatureSelector", # Does not work for regression
+        # "CARTFeatureSelector", # Only implemented for classification, OOM problems as well
+    ]
+)
+
+# Setup for CPU Methods (need to create another one for GPU models)
+TabArenaBenchmarkSetup(
+    # You could filter this to run less tasks
+    task_metadata=ALL_TASK_METADATA,
+    # Only run first three folds for now
+    split_indices_to_run=["r0f0"],
+    # Run methods for 5 configs (1 default + 4 random) each for now
+    n_random_configs=0,
+    models=[
+        ("LightGBM", "all"),
+        ("RandomForest", "all"),
+        ("Linear", "all"),
+    ],
+    preprocessing_pipelines=preprocessing_pipelines,
+    path_setup=UniPathSetupDominika(),
+).setup_jobs()
