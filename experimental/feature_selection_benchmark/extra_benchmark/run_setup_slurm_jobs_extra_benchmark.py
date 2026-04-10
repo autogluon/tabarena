5ef21ed4ada5a3f69c66741c9ee2124bdf99691b
bastis007 <schaefer.bastian@mail.de>
2026-04-10 15:54:58 +0200

    add: script for generating script for extra benchmark (needs to be tested)

---
+++ experimental/feature_selection_benchmark/extra_benchmark/run_setup_slurm_jobs_extra_benchmark.py
@@ -0,0 +1,53 @@
+"""Run TabArena for feature selection benchmark with downstream model performance evaluation."""
+
+from __future__ import annotations
+
+from fr_cluster_setup_extra import ALL_TASK_METADATA, FSBenchmarkConfig, TabArenaExtraBenchmarkSetup
+
+preprocessing_pipelines = FSBenchmarkConfig().get_default_preprocessing_configs(
+    fs_methods=[
+        "AccuracyFeatureSelector",
+        "RandomFeatureSelector",
+        "ANOVAFeatureSelector",
+        "CFSFeatureSelector",
+        "Chi2FeatureSelector",
+        "DISRFeatureSelector",
+        "GainRatioFeatureSelector",
+        "GiniFeatureSelector",
+        "ImpurityFeatureSelector",
+        "InformationGainFeatureSelector",
+        "INTERACTFeatureSelector",
+        "MarkovBlanketFeatureSelector",
+        "MIFeatureSelector",
+        "mRMRFeatureSelector",
+        "PearsonCorrelationFeatureSelector",
+        "ReliefFFeatureSelector",
+        "RFImportanceFeatureSelector",
+        "SequentialBackwardEliminationFeatureSelector",
+        "SequentialForwardSelectionFeatureSelector",
+        "SymmetricalUncertaintyFeatureSelector",
+        "LassoFeatureSelector",  # just for regression but with label encoder for classification?
+        "LaplacianScoreFeatureSelector",  # OOM, Segmentation fault issues
+        "ConsistencyFeatureSelector",
+        # selected_indices = np.where(S)[0].tolist(), UnboundLocalError: cannot access local variable 'S' where it is not associated with a value
+        "JMIFeatureSelector",
+        # time limit computed incorrectly, and error at remaining.remove(best_idx), ValueError: list.remove(x): x not in list
+        "OneRFeatureSelector",
+        # major OOM errors (tries to allocate one major array), wrong time limit computation,  max(accuracies, key=accuracies.get) -> max() iterable argument is empty
+        "ElasticNetFeatureSelector",  # Only for classification
+        "CMIMFeatureSelector",  # problems with time limit and fallback of features
+        # "tTestFeatureSelector", # Does not work for regression
+        "CARTFeatureSelector",  # Only implemented for classification, OOM problems as well
+    ]
+)
+
+# Setup for CPU Methods (need to create another one for GPU models)
+TabArenaExtraBenchmarkSetup(
+    # You could filter this to run less tasks
+    task_metadata=ALL_TASK_METADATA,
+    # Only run first fold for now
+    split_indices_to_run=["r0f0"],
+    # Run methods for 5 configs (1 default + 4 random) each for now
+    n_random_configs=0,
+    preprocessing_pipelines=preprocessing_pipelines,
+).setup_jobs()
