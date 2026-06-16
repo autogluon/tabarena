f2b512bdd50c5363331847cb0039196a55c6c693
LennartPurucker <lennart@priorlabs.ai>
2026-06-16 15:13:54 +0000

    add: run script for ChimeraBoost

--- packages/tabflow_slurm/experiments/run_setup_tabarena_v0pt1.py
+++ packages/tabflow_slurm/experiments/run_setup_tabarena_v0pt1.py
@@ -19,12 +19,9 @@ from tabflow_slurm import (
 )
 
 benchmark_plan = TabArenaBenchmarkPlan(
-    benchmark_name="example_tabarena_v0pt1_29052026",
+    benchmark_name="benchmark_chimeraboost_16062026",
     model_jobs=[
-        # GPU model: override the base (CPU-only) resources to request a GPU.
-        ModelJob(models=("TabPFN-3", 0), name="gpu", resources={"num_gpus": 1}),
-        # Example for CPU model: no resource override, so it runs on the base CPU resources.
-        ModelJob(models=("Linear", 1), name="cpu"),
+        ModelJob(models=("ChimeraBoost", 25)),
     ],
     tasks=TaskMetadataCollection.from_preset("TabArena-v0.1").subset_tasks(split_indices="lite"),
     experiment_bundle=TabArenaV0pt1ExperimentBundle(),
@@ -32,8 +29,30 @@ benchmark_plan = TabArenaBenchmarkPlan(
         workspace="/home/lennart_priorlabs_ai/workspace/benchmarking/tabarena_workspace",
         python_path="/home/lennart_priorlabs_ai/.venvs/beyondarena_27052026/bin/python",
     ),
-    resources_setup=TabArenaV0pt1ResourcesSetup(),
-    scheduler_setup=GCPSlurmSetup(),
+    # Run on GCP
+    # -> None for these two values so node values are picked up
+    # -> CPU partition: 16 vCPUs, 64 GB RAM, 0 GB VRAM
+    resources_setup=TabArenaV0pt1ResourcesSetup(memory_limit=None, num_cpus=None),
+    scheduler_setup=GCPSlurmSetup(cpu_partition="cpun416mtspotinteractive"),
 )
 
 benchmark_plan.setup_jobs()
+
+# # Example:
+# benchmark_plan = TabArenaBenchmarkPlan(
+#     benchmark_name="example_tabarena_v0pt1_29052026",
+#     model_jobs=[
+#         # GPU model: override the base (CPU-only) resources to request a GPU.
+#         ModelJob(models=("TabPFN-3", 0), name="gpu", resources={"num_gpus": 1}),
+#         # Example for CPU model: no resource override, so it runs on the base CPU resources.
+#         ModelJob(models=("Linear", 1), name="cpu"),
+#     ],
+#     tasks=TaskMetadataCollection.from_preset("TabArena-v0.1").subset_tasks(split_indices="lite"),
+#     experiment_bundle=TabArenaV0pt1ExperimentBundle(),
+#     path_setup=PathSetup(
+#         workspace="/home/lennart_priorlabs_ai/workspace/benchmarking/tabarena_workspace",
+#         python_path="/home/lennart_priorlabs_ai/.venvs/beyondarena_27052026/bin/python",
+#     ),
+#     resources_setup=TabArenaV0pt1ResourcesSetup(),
+#     scheduler_setup=GCPSlurmSetup(),
+# )
