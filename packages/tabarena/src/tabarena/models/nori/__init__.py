16b2cdcaf8d4dbbb345edb47435e04669593d0ac
LennartPurucker <lennart@priorlabs.ai>
2026-06-18 11:20:07 +0000

    add: noir model

---
+++ packages/tabarena/src/tabarena/models/nori/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from tabarena.models.nori.hpo import gen_nori
+from tabarena.models.nori.info import nori_info, nori_method_metadata
+
+__all__ = ["gen_nori", "nori_info", "nori_method_metadata"]
