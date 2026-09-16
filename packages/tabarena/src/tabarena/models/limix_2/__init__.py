e586bd6aef99f26c94d86a89cc727e27bffc182a
yangzijia <yangzijia777@gmail.com>
2026-09-16 22:17:48 +0800

    Add LimiX-2 Model

---
+++ packages/tabarena/src/tabarena/models/limix_2/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from tabarena.models.limix_2.hpo import gen_limix_2
+from tabarena.models.limix_2.info import limix_2_info, limix_2_method_metadata
+
+__all__ = ["gen_limix_2", "limix_2_info", "limix_2_method_metadata"]
